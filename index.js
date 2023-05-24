67c58f95c0ab0fefc8fdc5de4ee6bd9fee2218b4
Merritt Baggett <mertbagt@gmail.com>
2023-05-24 15:50:28 -0400

    website counter

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function () {
+  myFunction();
+};
 
 // Get the navbar
 var navbar = document.getElementById("navbar");
@@ -10,8 +12,22 @@ var sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
+let counterContainer = document.querySelector(".website-counter");
+let resetButton = document.querySelector("#reset");
+let visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
